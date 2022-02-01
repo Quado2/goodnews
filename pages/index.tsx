c3c0d7f2c9e1c16e301b997418073f9b1b62b535
Quado2 <engchikwas404@gmail.com>
2022-02-01 13:15:31 +0100

    check

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
   background-color: ${({ theme }) => theme.colorBackgroundPrimary};
 `;
 
