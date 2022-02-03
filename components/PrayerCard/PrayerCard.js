2aa4ae569e69a6e00061634e20824b924282b418
Quado2 <engchikwas404@gmail.com>
2022-02-03 13:31:48 +0100

    added more stuffs

--- components/PrayerCard/PrayerCard.js
+++ components/PrayerCard/PrayerCard.js
@@ -1,7 +1,17 @@
 import styled from 'styled-components'
 import Image from 'next/image'
 const Container = styled.div`
+width: 300px;
+height: 430px;
+position: relative;
+border: 1px solid ${({theme})=> theme.colorTextPrimary};
 
+div{
+position: absolute;
+bottom: 0;
+border: 1px solid red;
+width: 100%;
+}
 `
 
 
@@ -10,6 +20,9 @@ export default function PrayerCard({imageUrl,downloadUrl}){
 return(
 <Container>
   <Image src={imageUrl} />
+  <div>
+
+  </div>
 </Container>
 )
 }
