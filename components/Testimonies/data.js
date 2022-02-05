cd52568396e031afaa765eaf4c18b2122df80985
Quado2 <engchikwas404@gmail.com>
2022-02-05 23:13:46 +0100

    sorted all issues

---
+++ components/Testimonies/data.js
@@ -0,0 +1,44 @@
+import nigerian1 from "../../assets/images/nigerianman1.jpg";
+import nigerian2 from "../../assets/images/nigerian2.jpg";
+import nigerian3 from "../../assets/images/nigerian.jpg";
+import south from "../../assets/images/south.jpg";
+import malawi from "../../assets/images/malawi.webp";
+
+export const data = [
+  {
+    index: 0,
+    name: "Jacinta Akintoye",
+    image: nigerian2,
+    testimony:
+      "Trust in the LORD with all your heart, and do not lean on your own understanding. In all your ways acknowledge Him, and He will make straight your paths",
+  },
+
+  {
+    index: 1,
+    name: "Okoro David",
+    image: south,
+    testimony:
+      "Do nothing from selfish ambition or conceit, but in humility count others more significant than yourselves. Let each of you look not only to his own interests, but also to the interests of others.",
+  },
+  {
+    index: 2,
+    name: "Kwame Wedeh",
+    image: nigerian1,
+    testimony:
+      "Have I not commanded you? Be strong and courageous. Do not be frightened, and do not be dismayed, for the LORD your God is with you wherever you go.",
+  },
+
+  {
+    index: 3,
+    Name: "Kelvin Babafemi",
+    image: nigerian3,
+    testimony:
+      "And we know that for those who love God all things work together for good, for those who are called according to His purpose.",
+  },
+  {
+    index: 4,
+    name: "Ekrema Mayamiko",
+    image: malawi,
+    testimony: "God took me to the next level",
+  },
+];
