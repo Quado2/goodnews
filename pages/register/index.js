7b23737aa1734d6b8ef75551fb11ef24b391a96e
Quado2 <engchikwas404@gmail.com>
2022-02-14 23:28:31 +0100

    added header

--- pages/register/index.js
+++ pages/register/index.js
@@ -1,8 +1,19 @@
+<Head>
+        <title>Goodnews of Christ Baptist Church</title>
+        <meta
+          name="description"
+          content="Official Website of Goodnews of Christ Baptist Church, Awka."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+
 export default function Register(){
 
   return(
-    <>
-    
-    </>
+    <div>
+      register
+    </div>
+   
   )
 }
