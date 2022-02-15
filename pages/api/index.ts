60a2448a8ad100cddf975169822c71def17eccc6
Quado2 <engchikwas404@gmail.com>
2022-02-15 23:15:48 +0100

    setting up

---
+++ pages/api/index.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import mongoose from 'mongoose'
+
+
+type Data = {
+  name: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+
+  
+  
+  
+
+  res.status(200).json({ name: 'John Doe' })
+}
---
+++ utils/connection.ts
@@ -0,0 +1,27 @@
+import mongoose, {Model} from 'mongoose';
+
+
+//CONNECTING TO MONGOOSE
+const {DATABASE_URL} = process.env;
+
+//connection functions=
+
+export const connect = async () => {
+  const conn = await mongoose.connect(DATABASE_URL as string)
+    .catch(err =>console.log(err))
+  console.log("Connection estabalished")
+
+
+
+//Create a schema
+const UserSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+  disabled: Boolean,
+})
+
+const User = mongoose.models.UserScema || mongoose.model("UserSchema", UserSchema);
+
+return {conn, User}
+
+}
