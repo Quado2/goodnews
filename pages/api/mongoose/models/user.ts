00b69a195e4d2a9330340f5e8ace707c5542ec06
Quado2 <engchikwas404@gmail.com>
2022-02-18 00:00:41 +0100

    continued with api dev

--- pages/api/mongoose/models/user.ts
+++ pages/api/mongoose/models/user.ts
@@ -1,29 +1,53 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
 
-const userSchema = new Schema({
- 
-  name: {
+    email: {
+      type: String,
+      unique: true,
+    },
+
+    phone: {
+      type: String,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+export const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+const MemberSchema = new Schema({
+  firstName: {
     type: String,
     required: true,
   },
-
+  sureName: {
+    type: String,
+    required: true,
+  },
   email: {
     type: String,
-    unique: true,
+    required: true,
   },
-
   phone: {
     type: String,
-    unique: true,
+    required: true,
   },
   password: {
     type: String,
     required: true,
-    
-  }
-}, {timestamps:true});
+  },
+});
 
-
-export const User = mongoose.models.User || mongoose.model('User', userSchema);
+export const Member =
+  mongoose.models.Member || mongoose.model("Member", MemberSchema);
