26c98285b6ae3fbeeb26fb48f1c660882702febb
Quado2 <engchikwas404@gmail.com>
2022-02-24 23:54:09 +0100

    started creating logged in context

--- context/HambugerContext.js
+++ context/HambugerContext.js
@@ -4,10 +4,11 @@ export const HambugerContext = createContext();
 
 export default function HambugerContextProvider(props){
   const [clicked, setClicked] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
   
 
   return (
-    <HambugerContext.Provider value={{clicked, setClicked}}>
+    <HambugerContext.Provider value={{clicked, setClicked,loggedIn, setLoggedIn}}>
       {props.children}
     </HambugerContext.Provider>
   )
