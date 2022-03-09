a4f0e9bea45f850fff9470e113c16c75460ef46f
Quado2 <engchikwas404@gmail.com>
2022-03-09 19:48:35 +0100

    did titheSubmit

--- pages/api/graphql/resolvers/Mutation/tithes.ts
+++ pages/api/graphql/resolvers/Mutation/tithes.ts
@@ -1,5 +1,6 @@
 import { Context, TitheInput } from "../../../interfaces/interfaces";
 import dbConnect from "../../../mongoose/connection";
+import {Tithe} from '../../../mongoose/models'
 
 export const titheResolvers = {
   titheSubmit:  async (_: any, {tithe}: {tithe:TitheInput}, {userInfo}:Context) => {
@@ -15,6 +16,24 @@ export const titheResolvers = {
 
     
     const {amount, date, isConfirmed} = tithe;
-    
+
+    const newTithe = new Tithe({
+      amount,
+      date,
+      isConfirmed,
+      memberId: userInfo?.userId
+    })
+
+   const savedTithe =  await newTithe.save();
+   console.log(savedTithe);
+
+   const tithes = await Tithe.find({memberId: userInfo?.userId});
+
+
+   return {
+     userErrors: [],
+     tithes,
+   }
+
   }
 }
