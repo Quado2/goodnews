b6a89d6cbd454a7bba2ed3f699e74ed80ea8649e
Quado2 <engchikwas404@gmail.com>
2022-03-10 23:48:26 +0100

    started partnership

---
+++ pages/dashboard/partnership/index.tsx
@@ -0,0 +1,12 @@
+import DashboardLayout from "../../../HOC/DashboardLayout";
+
+
+const Partnership = () => {
+  return (
+    <DashboardLayout>
+      <h1 style={{color: "white"}}>Hello brothers</h1>
+    </DashboardLayout>
+  );
+};
+
+export default Partnership;
